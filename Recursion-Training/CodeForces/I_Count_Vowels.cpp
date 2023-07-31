fadb2878fc54b67ef8485c0b5c9fbe71cf9b80c6
Ed-Silverio-Mr-Booleano <edmilsondiabinho0@gmail.com>
2023-07-31 17:54:12 +0100

    feat: add problem I

--- Recursion-Training/CodeForces/I_Count_Vowels.cpp
+++ Recursion-Training/CodeForces/I_Count_Vowels.cpp
@@ -5,15 +5,19 @@ using namespace std;
 
 int c = 0;
 int countVowels(string s, int l){
-    if(l < 0) return c;
+    if(l < 1) return c;
     else{
+        s[l-1] = toupper(s[l-1]);
         if(s[l-1] == 'A' || s[l-1] == 'E' || s[l-1] == 'I' || s[l-1] == 'O' || s[l-1] == 'U' )
             c++;
-        countVowels(s, l-1);
+        return countVowels(s, l-1);
     }
 }
 int main(){
-    string s = "Data Structure Lab";
-    cout <<countVowels(s, s.length()-1)<<endl;
+    fast;
+    //freopen("I.in", "r", stdin);
+    string s;
+    getline(cin, s);
+    cout <<countVowels(s, s.length())<<endl;
     return 0;
 }
