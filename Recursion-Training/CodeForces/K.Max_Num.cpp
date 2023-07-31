ac0574c054bdcf3803d995f65843d7e24e257550
Ed-Silverio-Mr-Booleano <edmilsondaibinho0@gmail.com>
2023-07-31 22:02:05 +0100

    refactor: add new configs

--- Recursion-Training/CodeForces/K.Max_Num.cpp
+++ Recursion-Training/CodeForces/K.Max_Num.cpp
@@ -7,20 +7,22 @@ int c = 0, maior;
 ll maxNum(ll v[], int size){
     if(size < 0) return maior;
     else{
-        if(c == 0 ) maior = v[size-1];
-         else {
+        if(c == 0 ){
+            maior = v[size-1];
             c++;
-            if(maior > v[size]) maior = v[size];
-            maxNum(v, size -1);
         }
-        
+         else {
+            if(maior < v[size]) maior = v[size];
+            
+        }
+        maxNum(v, size -1);
     }
     
 }
 
 int main(){
     fast;
-    freopen("K.in", "r", stdin);
+    //freopen("K.in", "r", stdin);
     int n; cin>>n;
     ll v[n];
     for(ll i = 0, a; i < n; i++){
