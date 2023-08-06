ce60eb8d2e12510808ddb3dafff41dd9dd6c07e2
Ed-Silverio-Mr-Booleano <edmilsondiabinho0@gmail.com>
2023-08-06 16:37:04 +0100

    refactor: problem Z optize and change the complexity

--- Recursion-Training/CodeForces/Z_Left_Max.cpp
+++ Recursion-Training/CodeForces/Z_Left_Max.cpp
@@ -3,25 +3,28 @@
 #define ll long long
 
 using namespace std;
-int i = 0;
+int i = 0, n;
 int maior = 0;
+int j = 0;
 void leftMax(ll a[], int s){
     if(s < 1) return; 
-    for(int j = 0; j <=i ; j++){
-        if(j == 0) maior = a[j];
-        else{
-            if(maior < a[j]) maior = a[j];
-        }
+    
+    if(j == 0) maior = a[j];
+    else{
+        if(maior < a[j]) maior = a[j];
     }
+    if(j == n-1) j == 0;
+
     cout << maior<<" ";
     i++;
+    j++;
     leftMax(a, s-1);
 }
 int main(){
     fast;
-    //freopen("Z.in", "r", stdin);
+    freopen("Z.in", "r", stdin);
 
-    int n; cin>>n;
+    cin>>n;
     ll a[n];
 
     for(int i =0; i < n; i++)
